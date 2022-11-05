5310abc7c8ee72a561db32c29189851bfdc03073
SPENCER058 <galihpanji058@gmail.com>
2022-11-05 22:59:26 +0700

    Create_Galih

---
+++ app/src/main/java/com/example/biodatav3421048galih/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biodatav3421048galih;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
